49c7c85eee0b8f8e04b19a29dd8c683d3fd24000
Thysma <mathys.gaillet@gmail.com>
2024-03-07 09:32:59 +0100

    Card on homePage for vehicules

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -4,15 +4,19 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 class HomePageController extends AbstractController
 {
     #[Route('/homePage', name: 'app_home_page')]
     public function index(): Response
     {
+        $imageDirectory = $this->getParameter('kernel.project_dir') . '/assets/images/cars/';
+        $images = scandir($imageDirectory);
+        $images = array_diff($images, ['.', '..']); // Supprimer les entrées '.' et '..'
+
         return $this->render('homePage.html.twig', [
-            'controller_name' => 'HomePageController',
+            'images' => $images,
         ]);
     }
 }
